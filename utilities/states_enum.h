38f500e52be650f820121a1110560ba08c0b2668
Christopher Nielsen <c.consult.christopher@gmail.com>
2020-09-09 15:43:02 +0200

    init state machine

---
+++ utilities/states_enum.h
@@ -0,0 +1,13 @@
+#ifndef STATES_ENUM_H
+#define STATES_ENUM_H   
+
+typedef enum{
+    INIT,
+    LOAD_FILE,
+    RGB_TO_GRAY,
+    ANALOG_TO_DIGITAL,
+    EXIT
+}STATES;
+
+
+#endif // STATES_ENUM_H
---
+++ utilities/types.h
@@ -0,0 +1,11 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#define UCHAR unsigned char
+
+#define BOOL unsigned char
+#define TRUE 1
+#define FALSE 0
+
+
+#endif
