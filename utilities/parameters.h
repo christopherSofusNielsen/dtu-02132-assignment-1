14b841b559318e10d6300fa51524959762cef5bc
Christopher Nielsen <c.consult.christopher@gmail.com>
2020-09-16 16:36:47 +0200

    parameter change

--- utilities/parameters.h
+++ utilities/parameters.h
@@ -1,12 +1,15 @@
 #if !defined(PARAMETERS_H)
 #define PARAMETERS_H
-//110 works better
-#define GRAYSCALE_THRESHOLD 90
 
-#define CELL_SEARCH_RADIUS 5
+
+
+#define GRAYSCALE_THRESHOLD 110
+#define CELL_SEARCH_RADIUS 8
+
+
+
 
 #define POINTS_LENGTH 400
-
 #define CROSS_R_SIZE 8
 
 
